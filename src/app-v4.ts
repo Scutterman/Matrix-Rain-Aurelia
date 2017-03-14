518ebfb2c8122519c199bdf404e341343684a6ad
tom@cgfindies.co.uk <tom@cgfindies.co.uk>
2017-03-14 22:35:43 +0000

    Added comments.

--- src/app-v4.ts
+++ src/app-v4.ts
@@ -1,45 +1,61 @@
 import {computedFrom} from "aurelia-framework";
 
 export class App {
+    // The container that will hold the rain effect.
     private matrix: HTMLElement;
+
+    // The columns of characters as they fall down.
     private columns: MatrixColumn[] = new Array<MatrixColumn>();
 
+    // Computed values of characters within the container
     protected characterWidth: number;
     protected characterHeight: number;
     protected columnsOnScreen: number;
     protected rowsOnScreen: number;
 
-    protected minSpeed: number = 75;
-    protected maxSpeed: number = 150;
+    // Min / max spped that columns can move down the screen, represented as the number of pixels the column moves every time the app ticks.
+    protected minSpeed: number = 7.5;
+    protected maxSpeed: number = 15;
 
+    // Used to calculate when a column starts fading, as a multiplier of the number of characters in a full vertical column.
     protected minCharacterFactor: number = 0.1;
     protected maxCharacterFactor: number = 0.5;
     protected minCharacters: number;
     protected maxCharacters: number;
     
+    // Seconds between a column resetting and it starting to animate (delay phase).
     protected minColumnDelay: number = 0;
     protected maxColumnDelay: number = 10;
 
+    // The characters that might appear in a column.
     protected characters: string[] = new Array<string>();
     
     attached() {
+        // Push some characters into the available list.
         for (let i: number = 12449; i <= 12544; i++) { this.characters.push(String.fromCharCode(i)); }
 
+        // Calculate the character width and height of a monospaced character in the container. Also calculates the number of characters in a row and a column.
         this.setWidthsAndHeights(this.matrix);
         
+        // Fill the container with columns.
         for (let i: number = 0; i < this.columnsOnScreen; i++) { this.addColumn(); }
 
+        // Set the tick going every 25 miliseconds.
         setInterval(() => this.tick(), 25);
     }
     
     private tick() {
+        // Filter out the columns that are still in the delay phase. The rest need to tick.
         this.columns.filter(column => column.doTick === true).forEach((column: MatrixColumn) => {
+            // The MatrixColumn object does most of the work.
             column.tick();
 
+            // These columns have faded out, reset them for another run.
             if (column.charactersToRemove > (column.charactersToDisplay + 10)) { this.resetColumn(column); }
         });
     }
 
+    // Add a column to the container. Set default / static values, then reset it.
     private addColumn() {
         let column: MatrixColumn = new MatrixColumn();
         column.leftPosition = this.columns.length * this.characterWidth;
@@ -48,31 +64,39 @@ export class App {
         this.columns.push(column);
     }
 
+    // Get the column to reset itself, then set its text, speed, and delay phase duration.
     private resetColumn(column: MatrixColumn) {
         column.reset();
         column.setRowText(this.rowsOnScreen, this.minCharacters, this.characters);
-        column.pixelsPerTick = (App.getRandomNumberBetween(this.maxSpeed, this.minSpeed) / 10);
+        column.pixelsPerTick = App.getRandomNumberBetween(this.maxSpeed, this.minSpeed);
         setTimeout(() => {column.doTick = true}, App.getRandomNumberBetween(this.minColumnDelay, this.maxColumnDelay) * 1000)
     }
 
+    // Helper method to work around the limited nature of Javascript Math.random().
     public static getRandomNumberBetween(min: number, max: number) : number {
         return (Math.ceil(Math.random() * (max - min)) + min);
     }
     
     private setWidthsAndHeights(container: HTMLElement) {
+        // Add a test span with one character in it to the container
         let oneCharacterContainer: HTMLSpanElement = document.createElement('span');
-        
         oneCharacterContainer.innerText = String.fromCharCode(12449);
         oneCharacterContainer.setAttribute('style', 'visibility:hidden; font-family: ' + window.getComputedStyle(container, null).getPropertyValue('font-family') + ';');
         let addedNode: Node = container.appendChild(oneCharacterContainer)
 
+        // The width and height of the span maps pretty reliably to the width and height of that one character. Monospaced font is essential here.
         this.characterWidth = oneCharacterContainer.getBoundingClientRect().width;
         this.characterHeight = oneCharacterContainer.getBoundingClientRect().height;
+
+        // Simple math to get the number of rows and columns on the screen. Container dimensions divided by character dimensions. 
         this.columnsOnScreen = Math.floor(container.clientWidth / this.characterWidth) - 1;
         this.rowsOnScreen = Math.ceil(container.clientHeight / this.characterHeight) + 1;
+
+        // Number of characters on the screen before the top ones start fading.
         this.minCharacters = Math.ceil(this.rowsOnScreen * this.minCharacterFactor);
         this.maxCharacters = Math.ceil(this.rowsOnScreen * this.maxCharacterFactor);
 
+        // Remove the test span to make the container ready for the app to start.
         container.removeChild(addedNode);
     }
 }
