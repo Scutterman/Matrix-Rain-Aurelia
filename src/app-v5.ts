47ea10cb7b4cfa9a2535cbc29a215e66df97c278
Scutterman <=>
2017-03-17 15:57:08 +0000

    Re-wrote large portions of the app to put all animation in CSS transitions. Firefox and Edge performance are now bearable, Chrome performance still best.

--- src/app-v5.ts
+++ src/app-v5.ts
@@ -1,13 +1,8 @@
 import {computedFrom} from "aurelia-framework";
-import {inject} from 'aurelia-dependency-injection';
-import {TaskQueue} from 'aurelia-task-queue';
 
-@inject(TaskQueue)
 export class App {
     private static randomNumbers: number[] = new Array<number>();
     private static nextRandomNumberIndex: number = 0;
-
-    private tasks: TaskQueue;
 
     // The container that will hold the rain effect.
     private matrix: HTMLElement;
@@ -20,82 +15,57 @@ export class App {
     }
     
     // Computed values of characters within the container
-    protected characterWidth: number;
-    protected characterHeight: number;
-    protected columnsOnScreen: number;
     protected rowsOnScreen: number;
-
-    // Min / max spped that columns can move down the screen, represented as the number of pixels the column moves every time the app ticks.
-    protected minSpeed: number = 7.5;
-    protected maxSpeed: number = 15;
-
-    // Used to calculate when a column starts fading, as a multiplier of the number of characters in a full vertical column.
-    protected minCharacterFactor: number = 0.1;
-    protected maxCharacterFactor: number = 0.5;
-    protected minCharacters: number;
-    protected maxCharacters: number;
     
-    // Seconds between a column resetting and it starting to animate (delay phase).
-    protected minColumnDelay: number = 0;
-    protected maxColumnDelay: number = 10;
-
     // The characters that might appear in a column.
     protected characters: string[] = new Array<string>();
     
-    constructor(_tasks: TaskQueue)
-    {
-        this.tasks = _tasks;
-    }
-
     attached() {
+        
+        App.PopulateRandomNumbers();
+
         // Push some characters into the available list.
         for (let i: number = 12449; i < 12544; i++) { this.characters.push(String.fromCharCode(i)); }
 
         // Calculate the character width and height of a monospaced character in the container. Also calculates the number of characters in a row and a column.
-        this.setWidthsAndHeights(this.matrix);
+        this.setWidthsAndHeightsAndGenerateColumns(this.matrix);
         
-        App.PopulateRandomNumbers();
-
-        // Fill the container with columns.
-        this.tasks.queueMicroTask(() => {
-            for (let i: number = 0; i < this.columnsOnScreen; i++) { this.addColumn(); }
-        });
-
-        // Set the tick going every 25 miliseconds.
-        setInterval(() => this.tick(), 25);
+        
     }
     
-    private tick() {
-        this.tasks.queueMicroTask(() => {
-            // Filter out the columns that are still in the delay phase. The rest need to tick.
-            this.columns.filter(column => column.doTick === true).forEach((column: MatrixColumn) => {
-                // The MatrixColumn object does most of the work.
-                column.tick(this.tasks);
-
-                // These columns have faded out, reset them for another run.
-                if (column.charactersToRemove > (column.charactersToDisplay + 10)) { this.resetColumn(column); }
-            });
-        });    
-    }
-
     // Add a column to the container. Set default / static values, then reset it.
-    private addColumn() {
+    private addColumn(characterWidth: number, screenHeight: number) {
         let column: MatrixColumn = new MatrixColumn();
-        column.leftPosition = this.columns.length * this.characterWidth;
-        column.characterHeight = this.characterHeight;
+        column.leftPosition = this.columns.length * characterWidth;
+        column.screenHeight = screenHeight;
+        column.animationCompleteCallback = (mc: MatrixColumn) => { this.resetColumn(mc); };
         this.resetColumn(column);
         this.columns.push(column);
     }
 
     // Get the column to reset itself, then set its text, speed, and delay phase duration.
     private resetColumn(column: MatrixColumn) {
-        this.tasks.queueMicroTask(() => {
-            column.reset(this.tasks);
-            column.setRowText(this.rowsOnScreen, this.minCharacters, this.characters);
-            column.pixelsPerTick = App.getRandomNumberBetween(this.maxSpeed, this.minSpeed);
-        });
+        
+        // Min / max spped that columns can move down the screen, represented as the number of pixels the column moves every time the app ticks.
+        let minSpeed: number = 7.5;
+        let maxSpeed: number = 15;
+
+        // Milliseconds between a column resetting and it starting to animate (delay phase).
+        let minColumnDelay: number = 0;
+        let maxColumnDelay: number = 10000;
+
+        // Milliseconds before fade starts
+        let minColumnFadeDelay: number = 500;
+        let maxColumnFadeDelay: number = 1250;
+
+        let chars: string[] = new Array<string>();
+        for (let i: number = 0; i < this.rowsOnScreen; i++) {
+            chars.push(this.characters[Math.floor(App.getNextRandomNumber() * (this.characters.length))]);
+        }
+        
+        column.reset(/* delay */ App.getRandomNumberBetween(minColumnFadeDelay, maxColumnFadeDelay), /* ppt */ App.getRandomNumberBetween(minSpeed, maxSpeed), /* chars */ chars);
             
-        setTimeout(() => {column.doTick = true}, App.getRandomNumberBetween(this.minColumnDelay, this.maxColumnDelay) * 1000)
+        setTimeout(() => { column.startColumnAnimation(); }, App.getRandomNumberBetween(minColumnDelay, maxColumnDelay));
     }
 
     // Helper method to work around the limited nature of Javascript Math.random().
@@ -121,7 +91,7 @@ export class App {
         return nextNumber;
     }
 
-    private setWidthsAndHeights(container: HTMLElement) {
+    private setWidthsAndHeightsAndGenerateColumns(container: HTMLElement) {
         // Add a test span with one character in it to the container
         let oneCharacterContainer: HTMLSpanElement = document.createElement('span');
         oneCharacterContainer.innerText = String.fromCharCode(12449);
@@ -129,90 +99,79 @@ export class App {
         let addedNode: Node = container.appendChild(oneCharacterContainer)
 
         // The width and height of the span maps pretty reliably to the width and height of that one character. Monospaced font is essential here.
-        this.characterWidth = oneCharacterContainer.getBoundingClientRect().width;
-        this.characterHeight = oneCharacterContainer.getBoundingClientRect().height;
+        let characterWidth: number = oneCharacterContainer.getBoundingClientRect().width;
+        let characterHeight: number = oneCharacterContainer.getBoundingClientRect().height;
 
         // Simple math to get the number of rows and columns on the screen. Container dimensions divided by character dimensions. 
-        this.columnsOnScreen = Math.floor(container.clientWidth / this.characterWidth) - 1;
-        this.rowsOnScreen = Math.floor(container.clientHeight / this.characterHeight) + 10;
-
-        // Number of characters on the screen before the top ones start fading.
-        this.minCharacters = Math.ceil(this.rowsOnScreen * this.minCharacterFactor);
-        this.maxCharacters = Math.ceil(this.rowsOnScreen * this.maxCharacterFactor);
+        let columnsOnScreen: number = Math.floor(container.clientWidth / characterWidth) - 1;
+        this.rowsOnScreen = Math.floor(container.clientHeight / characterHeight) + 30;
+        let screenHeight: number = this.rowsOnScreen * characterHeight;
 
         // Remove the test span to make the container ready for the app to start.
         container.removeChild(addedNode);
+
+        for (let i: number = 0; i < columnsOnScreen; i++) { this.addColumn(characterWidth, screenHeight); }
     }
 }
 
 export class MatrixColumn {
-    public characterHeight: number;
     public leftPosition: number;
     public topPositioning: number = 0;
     public columnHeight: number = 0;
-    public pseudoHeight: number = 0;
     
-    public charactersToDisplay: number = 0;
-    public charactersToRemove: number = 0;
-    public charactersBeforeFadeStarts: number = 0;;
-    public pixelsPerTick: number;
+    public secondsToAnimate: number;
+    public fadeDelay: number;
     
-    public columnCharacters: string[] = new Array<string>();
+    public transition: number = 0;
+    public fadeTransition: number = 0;
+    public screenHeight: number = 0;
 
-    public doTick: boolean = false;
+    public _columnText: string = "";
+
+    public animationCompleteCallback: (mc: MatrixColumn) => any = null;
 
     @computedFrom("columnHeight")
     get cssText(): string {
-        return `left: ${this.leftPosition}px; height: ${this.columnHeight}px;`;
+        return `left: ${this.leftPosition}px; height: ${this.columnHeight}px; transition: height ${this.transition}s;`;
     }
     
     @computedFrom("topPositioning")
     get cssTextFadeColumn(): string {
-        return `left: ${this.leftPosition}px; height: ${this.topPositioning}px;`;
+        return `left: ${this.leftPosition}px; height: ${this.topPositioning}px; transition: height ${this.fadeTransition}s;`;
     }
     
-    public _columnText: string = "";
-
     @computedFrom("_columnText")
     get columnText(): string {
         return this._columnText;
     }
 
-    public reset(tasks: TaskQueue){
-        this.doTick = false;
-        this.pseudoHeight = 0;
+    public reset(delay: number, ppt: number, chars: string[]) {
         this.topPositioning = 0;
-        this.charactersToDisplay = 0;
-        this.charactersToRemove = 0;
         this.columnHeight = 0;
+
+        this.transition = 0;
+        this.fadeTransition = 0;
+
+        this._columnText = chars.join('<br />');
+        
+        this.fadeDelay = delay;
+        let pixelsPerSecond: number = ppt * 40;
+        
+        // How long it will take to animate a fill column drop.
+        this.secondsToAnimate = Math.floor(this.screenHeight / pixelsPerSecond);
     }
 
-    public setRowText(rowsOnScreen: number, minCharacters: number, characters: string[]) {
-        this.charactersBeforeFadeStarts = App.getRandomNumberBetween(rowsOnScreen, minCharacters);
-
-        this.columnCharacters = new Array<string>();
-        for (let i: number = 0; i < rowsOnScreen; i++) {
-            this.columnCharacters.push(characters[Math.floor(App.getNextRandomNumber() * (characters.length))]);
-        }
-
-        this._columnText = this.columnCharacters.join('<br />');
+    public startColumnAnimation() {
+        this.transition = this.secondsToAnimate;
+        this.columnHeight = this.screenHeight;
+        setTimeout(() => { this.startFadeColumnAinimation(); }, this.fadeDelay)
     }
 
-    public tick(tasks: TaskQueue){
-        if (this.charactersToDisplay < this.columnCharacters.length)
-        {
-            this.pseudoHeight += this.pixelsPerTick;
-            this.charactersToDisplay = Math.min(this.columnCharacters.length, Math.floor(this.pseudoHeight / this.characterHeight));
-            this.columnHeight = this.charactersToDisplay * this.characterHeight;
-        }
-            
-        if (this.charactersToDisplay >= this.charactersBeforeFadeStarts)
-        {
-            // This is essentially like having a box overlay the column, growing in height every tick and covering characters.
-            this.topPositioning += this.pixelsPerTick;
-
-            // The total number of characters that would at least partially covered by the pseudo overlay.
-            this.charactersToRemove = Math.ceil(this.topPositioning / this.characterHeight);
+    public startFadeColumnAinimation() {
+        this.fadeTransition = (this.secondsToAnimate * 1);
+        this.topPositioning = this.screenHeight;
+        if (this.animationCompleteCallback != null) {
+            setTimeout(() => { this.animationCompleteCallback(this); }, this.secondsToAnimate * 1000);
         }
     }
 
